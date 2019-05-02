7ececc43fa6afd01a2bc69562c814b57b9322820
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-05-02 23:08:15 +0100

    Styling the VideoDetail

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail'
+
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -16,16 +18,19 @@ class App extends React.Component {
         this.setState({ videos: response.data.items }); 
     };
 
-    onVideoSelect = (video) => {
-        console.log('From the App!', video);
-        
+    onVideoSelect = video => {
+        this.setState({ selectedVideo: video }); 
     };
 
     render() {
          return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={this.onVideoSelect}
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
